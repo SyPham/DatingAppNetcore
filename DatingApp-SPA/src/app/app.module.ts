9ec7415e0e131ff3d93a2d2d01a885cd23789280
Henry Pham <phamtiensy8696@gmail.com>
2019-10-28 23:56:16 +0700

    The end part 5

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HttpClient } from 'selenium-webdriver/http';
-//import http client
+
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
+
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.Interceptor';
+
 
 
 @NgModule({
@@ -24,7 +25,7 @@ import { RegisterComponent } from './register/register.component';
       FormsModule
    ],
    providers: [
-      AuthService
+      ErrorInterceptorProvider
    ],
    bootstrap: [
       AppComponent
